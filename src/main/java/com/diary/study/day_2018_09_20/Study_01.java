633763c6f2265085af435eddbd6748b3fd7c66c9
Faydan <fei321457749@126.com>
2018-09-20 11:13:32 +0800

    提交人: 郭鹏飞 提交内容: 项目初始化

---
+++ src/main/java/com/diary/study/day_2018_09_20/Study_01.java
@@ -0,0 +1,25 @@
+package com.diary.study.day_2018_09_20;
+
+import com.diary.study.day_2018_09_20.entity.People.People;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Study_01 {
+
+    public static void main(String[] args) {
+        List<People> list = new ArrayList<>();
+        list.add(new People(1, "小明"));
+        list.add(new People(2, "小明"));
+        list.add(new People(3, "小黑"));
+        list.add(new People(4, "小花"));
+
+        // 遍历删除数组中的指定元素
+        list.removeIf(people -> people.getName().equals("小明"));
+
+        list.forEach(System.out::println);
+
+
+    }
+}
